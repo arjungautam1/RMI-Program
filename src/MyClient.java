d87bcf4ebd3842f6111f45bc6646122f6ef5b20d
arjungautam1 <arjungautam5431@gmail.com>
2022-01-04 17:45:58 +0545

    Step 4 : Create and run the clint application .

--- src/MyClient.java
+++ src/MyClient.java
@@ -1,3 +1,5 @@
+import java.rmi.Naming;
+
 /**
  * Created By Arjun Gautam
  * Date :04/01/2022
@@ -6,4 +8,12 @@
  */
 
 public class MyClient {
+    public static void main(String args[]) throws Exception{
+
+        Adder a=(Adder) Naming.lookup("rmi://localhost:5000/as");
+
+        int s=a.add(20,20);
+        System.out.println("Sum ="+s);
+
+    }
 }
