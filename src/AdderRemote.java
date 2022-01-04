ab86ea3bbdb8ecbfa412fe0250d05c9bdfa2afea
arjungautam1 <arjungautam5431@gmail.com>
2022-01-04 16:55:49 +0545

    RMI Program

---
+++ src/AdderRemote.java
@@ -0,0 +1,9 @@
+/**
+ * Created By Arjun Gautam
+ * Date :04/01/2022
+ * Time :4:54 PM
+ * Project Name :RMIDemo
+ */
+
+public class AdderRemote {
+}
---
+++ src/MyServer.java
@@ -0,0 +1,9 @@
+/**
+ * Created By Arjun Gautam
+ * Date :04/01/2022
+ * Time :4:55 PM
+ * Project Name :RMIDemo
+ */
+
+public class MyServer {
+}
