c931b799c086d5e049f72af2ddd42a8f6b38a3bb
arjungautam1 <arjungautam5431@gmail.com>
2022-01-04 17:14:48 +0545

    Step 3 : Create and run the server application.

--- src/MyServer.java
+++ src/MyServer.java
@@ -1,3 +1,6 @@
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+
 /**
  * Created By Arjun Gautam
  * Date :04/01/2022
@@ -6,4 +9,13 @@
  */
 
 public class MyServer {
+    public static void main(String[] args) {
+        try {
+            Adder adder = new AdderRemote();
+            Naming.rebind("rmi://localhost:5000/rmiserver", adder);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
